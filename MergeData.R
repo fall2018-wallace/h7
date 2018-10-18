d7481a52b163307524a8bda18bc7308ef610ea0e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:42:21 -0400

    Saving MergeData.R

--- MergeData.R
+++ MergeData.R
@@ -1,3 +1,13 @@
 
 library(ggplot2)
 library(ggmap)
+
+localUSArrests<-USArrests
+localUSArrests
+df
+df<-df[-9,]
+row.names(localUSArrests)
+colnames(df)
+localUSArrests$stateName <- df$stateName  
+df <- merge(df, localUSArrests, by = "stateName")   #merging by the common column stateName
+df       
