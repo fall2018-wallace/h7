a13fff24ebdadc53966af9e589cb6114a341462e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:00:03 -0400

    Saving MergeData.R

--- MergeData.R
+++ MergeData.R
@@ -17,7 +17,7 @@ statname <- localUSArrests$stateName
 area <- state.area
 center <- state.center
 
-merge_df <- data.frame(statname, area, center)
+merge_df <- data.frame(statename, area, center)
 
 merge_data <- merge(df, merge_df )
 
